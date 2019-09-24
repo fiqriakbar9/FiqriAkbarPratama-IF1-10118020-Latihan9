ebe2a5688376408ce5dded631d89599313a28779
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-09-24 21:00:20 +0700

    Add files via upload

---
+++ src/teks/Teks.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teks;
+
+/**
+ *
+ * Nama : Fiqri Akbar Pratama
+ * NIM  : 10118020  
+ * Kelas: IF-1 
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis" teks
+ * sesuai dengan fungsinya
+ * 
+ */
+
+public class Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+                
+    }
+    
+}
